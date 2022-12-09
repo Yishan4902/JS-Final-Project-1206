768e82ceea93e7695c47ad71a33b786a9115f851
Yishan4902 <119276617+Yishan4902@users.noreply.github.com>
2022-12-09 16:23:01 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,9 @@
-const api_path="project4902"
-const token="gP93r2xqw2fMVqMECxloCxEa3Bo2"
-
-
+const api_path="project4902";
+const token="gP93r2xqw2fMVqMECxloCxEa3Bo2";
+const config = {
+    headers: {
+      "Authorization":token,
+    }
+   };
 
 
